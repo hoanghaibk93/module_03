4b9cf6a4d223a31ecf9216ccf43338724489b906
hoanghaibk93 <hoanghaibk93@gmail.com>
2023-02-23 14:47:58 +0700

    first commit

--- co_so_du_lieu_quan_he/bai_tap/student_management.sql
+++ co_so_du_lieu_quan_he/bai_tap/student_management.sql
@@ -1,30 +1,32 @@
 create database student_management;
-create table Class(
+use student_management;
+create table class(
 id int primary key,
 name varchar(300)
 );
-insert into Class(id, name)
+insert into class(id, name)
 value (1, 'Huy'),
 (2, 'Hung'),
 (3, 'Anh'),
 (4, 'Phuc');
-select * from Class;
-Drop table Class;
+select * from class;
+Drop table class;
 DRop database student_management;
-create table Teacher(
+create table teacher(
 id int primary key auto_increment,
 name varchar(200),
 age int,
 country varchar(300)
 );
-insert into Teacher(name, age, country)
+Drop table teacher;
+insert into teacher(name, age, country)
 value ('Quynh',20,'Viet Nam'),
 ('Mr.Smirth',25,'English'),
 ('Miss.Thu',30,'French'),
 ('Miss.Huong',30,'French');
-delete from Teacher
+delete from teacher
 where id > 2;
-select * from Teacher;
-update Teacher
+select * from teacher;
+update teacher
 set age = 40
 where id = 2;
