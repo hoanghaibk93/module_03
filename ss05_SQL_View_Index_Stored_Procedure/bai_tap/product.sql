16e9f881d054d041951c081d43701a77a1ce2c41
hoanghaibk93 <hoanghaibk93@gmail.com>
2023-02-28 22:58:05 +0700

    update

--- ss05_SQL_View_Index_Stored_Procedure/bai_tap/product.sql
+++ ss05_SQL_View_Index_Stored_Procedure/bai_tap/product.sql
@@ -36,11 +36,36 @@ begin
  select * from products;
  end $$
 delimiter ;
- call method_products()
+ call method_products();
 delimiter $$
-create procedure and_method_products()
+create procedure and_method_products(in id int,in product_code varchar(10), in product_name varchar(30), in product_price double, in product_amount int, in product_description varchar(99), in product_status bit)
 begin 
- select * from products;
+ insert into products
+ values (id, product_code, product_name, product_price, product_amount, product_description, product_status);
  end $$
 delimiter ; 
- 
+call and_method_products(6, 'xxvl-006','tivi',500000,5,'tivi samsung', 0);
+call and_method_products(7, 'xxvl-009','tivi',500000,5,'tivi samsung', 0);
+ delimiter $$
+create procedure update_method_products(in by_id int,in new_product_code varchar(10), in new_product_name varchar(30), in new_product_price double, in new_product_amount int, in new_product_description varchar(99), in new_product_status bit)
+begin 
+ update products
+ set
+ product_code = new_product_code,
+ product_name = new_product_name ,
+ product_price = new_product_price,
+ product_amount = new_product_amount,
+ product_description = new_product_description,
+ product_status = new_product_status
+ where id = by_id;
+ end $$
+delimiter ; 
+call update_method_products(6,'xxvl-010','tủ lạnh',540000,7,'samsung', 1);
+ delimiter $$
+create procedure delete_method_products(in by_id int)
+begin 
+ delete from products
+ where id = by_id;
+ end $$
+delimiter ; 
+call delete_method_products(4);
