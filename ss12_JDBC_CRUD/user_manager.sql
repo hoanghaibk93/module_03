5efe4e739a3a7ec6001076229ab156512e535d93
hoanghaibk93 <hoanghaibk93@gmail.com>
2023-03-15 20:34:57 +0700

    update

--- ss12_JDBC_CRUD/user_manager.sql
+++ ss12_JDBC_CRUD/user_manager.sql
@@ -13,3 +13,23 @@ value('Huy','huy.hoang@gmail.com','VietNam'),
 ('Tung','tung.pham@gmail.com','Duc'),
 ('Thu','thu.pham@gmail.com','Lao'),
 ('Nhu','nhu.pham@gmail.com','VietNam');
+
+DELIMITER $$
+CREATE procedure findAll()
+begin
+select * 
+from users;
+end; $$
+DELIMITER ;
+call findAll();
+Delimiter $$
+create procedure updateUser(in new_name varchar(30), in new_email varchar(99), in new_country varchar(99), in new_id int)
+begin
+update users
+set name = new_name,
+email = new_email,
+country = new_country
+where id = new_id;
+end; $$
+delimiter ;
+call updateUser('Trung Hoành','hoang.trung@gmail.com','phap',9)
